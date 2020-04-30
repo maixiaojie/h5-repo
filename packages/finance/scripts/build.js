3495662a7085f6c925a1224a84af417cfbf4f976
wenyujie <wenyujie@hundun.cn>
2020-04-30 17:02:51 +0800

    feat: release tool

--- packages/finance/scripts/build.js
+++ packages/finance/scripts/build.js
@@ -2,10 +2,11 @@ const path = require('path')
 const shell = require('shelljs')
 const Bundler = require('parcel-bundler')
 const bs = require('browser-sync').create()
+const glob = require('glob-promise')
 
 const cwd = process.cwd()
 const { name, parcel, browsersync, hundun } = require(path.join(cwd, 'package.json'))
-
+const DIST_PATH = path.join(cwd, 'dist')
 const rm = files => shell.rm('-rf', files.map(file => `${cwd}/${file}`))
 
 const handleParcel = async (entry, options = {}) => {
@@ -14,10 +15,11 @@ const handleParcel = async (entry, options = {}) => {
 }
 const handleBrowserSync = (options = {}) => {
     bs.init({
-      ...options
+        ...options
     })
-  }
-  
+}
+
+
 const action = process.argv[2]  // dev build
 
 const dev = (env = 'development') => {
@@ -40,6 +42,23 @@ const build = (env = 'production') => {
     rm([options.outDir, '.cache'])
     handleParcel(entryFiles, options)
 }
+const putToServer = files => {
+    const fileStr = files.join(' ')
+    if (!fileStr) return
+
+    const server_path = 'root@172.96.199.92'
+    const object_dir = `/web/h5/${name}/`
+
+    shell.cd('dist')
+
+    shell.exec(`scp ${fileStr} ${server_path}:${object_dir}`, { silent: false }, (code, stdout, stderr) => {
+        console.log(code, stdout, stderr)
+    })
+}
+const release = async () => {
+    const files = await glob('*', { cwd: DIST_PATH })
+    putToServer(files)
+}
 
 switch (action) {
     case 'dev':
@@ -47,6 +66,9 @@ switch (action) {
         break;
     case 'build':
         build();
+        break;
+    case 'release':
+        release();
         break;
     default:
         break;
